71303e7f91357d06f5dbebe0604e2889c8cf3c53
Khor Kean Teng <u2004763@siswa.um.edu.my>
2025-05-26 18:24:59 +0800

    update

---
+++ merge.sh
@@ -0,0 +1,3 @@
+# require nbformat and nbmerge
+export PATH="$PATH:/C/Users/Khor Kean Teng/AppData/Local/Programs/Python/Python312/Scripts"
+nbmerge task_1.ipynb task_2.ipynb task_3.ipynb > WQD7005_A1_Khor_Kean_Teng.ipynb
